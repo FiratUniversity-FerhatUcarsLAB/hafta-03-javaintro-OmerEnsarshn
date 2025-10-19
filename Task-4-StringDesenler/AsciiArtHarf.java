4b65d2c7438d0e84d0322d0137ccdde119406163
OmerEnsarshn <250542022@firat.edu.tr>
2025-10-19 13:37:01 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("  OOO  ");
+        System.out.println(" O   O ");
+        System.out.println("O     O");
+        System.out.println("O     O");
+        System.out.println(" O   O ");
+        System.out.println("  OOO  ");
+    }
+}
