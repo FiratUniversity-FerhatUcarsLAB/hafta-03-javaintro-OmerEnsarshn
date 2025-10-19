f0fabffb03efe60b85965d266db5cb325c08106e
OmerEnsarshn <250542022@firat.edu.tr>
2025-10-19 12:58:59 +0300

    Add BesKereTekrar.java to print welcome message

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hos Geldniniz");
+        System.out.println("Java'ya Hos Geldniniz");
+        System.out.println("Java'ya Hos Geldniniz");
+        System.out.println("Java'ya Hos Geldniniz");
+        System.out.println("Java'ya Hos Geldniniz");
+
+    }
+}
