0738bbd58d8c2d9aab44399693a664a370c18ba0
OmerEnsarshn <250542022@firat.edu.tr>
2025-10-19 13:34:25 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        String ad = " Omer Ensar ";
+        String soyad = " Sahin ";
+        String ogrNo = " 250542022 ";
+        String bolum = " Yazilim Muh .";
+
+        System . out . println (" =================== ") ;
+        System . out . println (" OGRENCI BILGILERI ") ;
+        System . out . println (" =================== ") ;
+        System . out . println ("Ad : " + ad ) ;
+        System . out . println ("Soyad : " + soyad ) ;
+        System . out . println ("No : " + ogrNo ) ;
+        System . out . println ("Bolum : " + bolum ) ;
+        System . out . println (" =================== ") ;
+    }
+}
