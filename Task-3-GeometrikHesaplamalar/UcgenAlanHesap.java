4e9226931d6ca7ff15451cf312f4d3f74556efa8
OmerEnsarshn <250542022@firat.edu.tr>
2025-10-19 13:28:00 +0300

    Update UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        double a = 3, b = 4, c = 5;
+        double s = (a + b + c) / 2; // yarı çevre
+        double alan = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+        System.out.println("Ucgen Alani: " + alan);
+        System.out.println("Ucgen Çevresi: " + (a + b + c));
+    }
+};
