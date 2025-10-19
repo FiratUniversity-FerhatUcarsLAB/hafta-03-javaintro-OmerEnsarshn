bdd45d7bcc1774986755bd1841d45b7813ce6010
OmerEnsarshn <250542022@firat.edu.tr>
2025-10-19 13:48:25 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int sayi = 2;
+
+        System.out.println("2'nin Carpim Tablosu:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("2 x %d = %d\n", i, sayi * i);
+        }
+    }
+}
