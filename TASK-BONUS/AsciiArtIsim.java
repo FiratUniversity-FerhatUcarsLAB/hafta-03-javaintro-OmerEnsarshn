cf54b4e496581c8e499105dc1005269ed78e42f1
OmerEnsarshn <250542022@firat.edu.tr>
2025-10-23 14:49:36 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println(" OOO   M   M  EEEEE  RRRR        EEEEE  N   N  SSSS   A    RRRR ");
+        System.out.println("O   O  MM MM  E      R   R       E      NN  N  S     A A   R   R");
+        System.out.println("O   O  M M M  EEEE   RRRR        EEEE   N N N  SSSS  AAAA  RRRR ");
+        System.out.println("O   O  M   M  E      R R         E      N  NN     S  A  A  R R  ");
+        System.out.println(" OOO   M   M  EEEEE  R  RR       EEEEE  N   N  SSSS  A  A  R  RR");
+    }
+}
